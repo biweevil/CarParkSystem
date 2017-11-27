b4c183241ec27bed88dd3a7c026138aa3782c593
biweevil <jordan4rams@gmail.com>
2017-11-27 05:46:02 +0000

    Gui Fixed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,15 +10,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainGUI.fxml"));
+        Parent root = (Parent) loader.load();
+        Controller controller = loader.getController();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
-
     }
 }
