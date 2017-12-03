6803abee39b45cba45f00eac594faaf37575cc14
biweevil <jordan4rams@gmail.com>
2017-12-03 12:26:54 +0000

    Improved GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,15 +18,9 @@ public class Main extends Application {
         Parent root = (Parent) loader.load();
         Controller controller = loader.getController();
         primaryStage.setTitle("Car Park System");
-        Scene main = new Scene(root, 3840, 2160);
+        Scene main = new Scene(root, 1200, 700);
         main.getStylesheets().add(Main.class.getResource("css.css").toExternalForm());
         primaryStage.setScene(main);
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        primaryStage.setX(primaryScreenBounds.getMinX());
-        primaryStage.setY(primaryScreenBounds.getMinY());
-        primaryStage.setWidth(primaryScreenBounds.getWidth());
-        primaryStage.setHeight(primaryScreenBounds.getHeight());
-        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
